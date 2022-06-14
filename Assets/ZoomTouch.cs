d9a36430b9827a822387a2ce58ca93fdb89ac2a2
PastryChew <80216822+PastryChew@users.noreply.github.com>
2022-06-14 18:05:53 +0300

    29.6

--- Assets/ZoomTouch.cs
+++ Assets/ZoomTouch.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class ZoomTouch : MonoBehaviour
 {
-    private float tr;
-    private Vector2 touchpos1;
-    private Vector2 touchpos2;
+    private float _tr;
+    private Vector2 _touchpos1;
+    private Vector2 _touchpos2;
     private void Update()
     {
         if (Input.touchCount > 1)
         {
             Touch touch1 = Input.GetTouch(0);
             Touch touch2 = Input.GetTouch(1);
-            touchpos1 = new Vector2(touch1.position.x, touch1.position.y);
-            touchpos2 = new Vector2(touch2.position.x, touch2.position.y);
+            _touchpos1 = new Vector2(touch1.position.x, touch1.position.y);
+            _touchpos2 = new Vector2(touch2.position.x, touch2.position.y);
             if (touch1.phase == TouchPhase.Began && touch2.phase == TouchPhase.Began)
             {
-                tr = Vector2.Distance(touchpos1, touchpos2);
+                _tr = Vector2.Distance(_touchpos1, _touchpos2);
             }
-            if (tr < Vector2.Distance(touchpos1, touchpos2))
+            if (_tr < Vector2.Distance(_touchpos1, _touchpos2))
             {
                 Debug.Log("Жест увеличение");
             }
-            else if (tr > Vector2.Distance(touchpos2, touchpos1)) Debug.Log("Жест уменьшения");
+            else if (_tr > Vector2.Distance(_touchpos2, _touchpos1)) Debug.Log("Жест уменьшения");
         }
     }
 }
